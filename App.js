8ce6f0ee17930c969fed45527689968fb68d1022
Usman Kiani <Usmankianiofficial@gmail.com>
2020-09-01 10:01:30 +0500

    updated

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {View, TouchableOpacity, Text, Image} from 'react-native';
 import {Root} from './src/config/layouts';
-import {BorderlessButton} from './src/components/BorderlessButton';
 
 export default class App extends React.Component {
-  state = {
-    isLoading: false,
-  };
   render() {
-    const {isLoading} = this.state;
     return (
       <>
-        <Root>
-          <BorderlessButton
-            // main
-            style={
-              {
-                // Custom Styling
-              }
-            }
-            text="Next"
-            isLoading={isLoading}
-            onPress={() => {
-              if (isLoading) {
-                // this.setState({isLoading: !isLoading});
-              } else {
-                this.setState({isLoading: !isLoading});
-              }
-            }}
-          />
-        </Root>
+        <Root></Root>
       </>
     );
   }
